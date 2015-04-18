992c8890df449ae48675e9d3806b80d066cdd99a
Revan Sopher <rsopher@gmail.com>
2015-04-18 17:09:53 -0400

    Remove date from watchface.

--- server/simple_analog/src/simple_analog.c
+++ server/simple_analog/src/simple_analog.c
@@ -3,9 +3,7 @@
 #include "pebble.h"
 
 static Window *window;
-//static Layer *s_simple_bg_layer, *s_date_layer, *s_hands_layer;
-static Layer *s_date_layer, *s_hands_layer;
-static TextLayer *s_day_label, *s_num_label;
+static Layer *s_hands_layer;
 
 // background
 static BitmapLayer *s_background_layer;
@@ -13,7 +11,6 @@ static GBitmap *s_background_bitmap;
 
 static GPath *s_tick_paths[NUM_CLOCK_TICKS];
 static GPath *s_minute_arrow, *s_hour_arrow;
-static char s_num_buffer[4], s_day_buffer[6];
 
 static void bg_update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorBlack);
@@ -58,17 +55,6 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
   graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
 }
 
-static void date_update_proc(Layer *layer, GContext *ctx) {
-  time_t now = time(NULL);
-  struct tm *t = localtime(&now);
-
-  strftime(s_day_buffer, sizeof(s_day_buffer), "%a", t);
-  text_layer_set_text(s_day_label, s_day_buffer);
-
-  strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
-  text_layer_set_text(s_num_label, s_num_buffer);
-}
-
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   layer_mark_dirty(window_get_root_layer(window));
 }
@@ -83,26 +69,6 @@ static void window_load(Window *window) {
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
-  s_date_layer = layer_create(bounds);
-  layer_set_update_proc(s_date_layer, date_update_proc);
-  layer_add_child(window_layer, s_date_layer);
-
-  s_day_label = text_layer_create(GRect(46, 114, 27, 20));
-  text_layer_set_text(s_day_label, s_day_buffer);
-  text_layer_set_background_color(s_day_label, GColorBlack);
-  text_layer_set_text_color(s_day_label, GColorWhite);
-  text_layer_set_font(s_day_label, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-
-  layer_add_child(s_date_layer, text_layer_get_layer(s_day_label));
-
-  s_num_label = text_layer_create(GRect(73, 114, 18, 20));
-  text_layer_set_text(s_num_label, s_num_buffer);
-  text_layer_set_background_color(s_num_label, GColorBlack);
-  text_layer_set_text_color(s_num_label, GColorWhite);
-  text_layer_set_font(s_num_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-
-  layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
-
   s_hands_layer = layer_create(bounds);
   layer_set_update_proc(s_hands_layer, hands_update_proc);
   layer_add_child(window_layer, s_hands_layer);
@@ -115,12 +81,6 @@ static void window_unload(Window *window) {
   // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
 
-  //layer_destroy(s_simple_bg_layer);
-  layer_destroy(s_date_layer);
-
-  text_layer_destroy(s_day_label);
-  text_layer_destroy(s_num_label);
-
   layer_destroy(s_hands_layer);
 }
 
@@ -131,9 +91,6 @@ static void init() {
     .unload = window_unload,
   });
   window_stack_push(window, true);
-
-  s_day_buffer[0] = '\0';
-  s_num_buffer[0] = '\0';
 
   // init hand paths
   s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
